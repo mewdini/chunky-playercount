e45ce5b7efd26b61d29c57d1f6e2c583ad2db789
Pablo Solano <pasolano@email.wm.edu>
2023-04-12 23:16:43 -0700

    Added basic automatic continue/resume

--- src/main/java/codes/pablo/chunky/ChunkyPlayerCount.java
+++ src/main/java/codes/pablo/chunky/ChunkyPlayerCount.java
@@ -4,17 +4,16 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-
-import org.popcraft.chunky.api.ChunkyAPI;
-
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
-import net.minecraft.util.WorldSavePath;
+import net.minecraft.world.World;
 
+import org.popcraft.chunky.api.ChunkyAPI;
 import org.popcraft.chunky.ChunkyProvider;
 
 public class ChunkyPlayerCount implements ModInitializer {
@@ -23,44 +22,46 @@ public class ChunkyPlayerCount implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("ChunkyPlayerCount");
 	private static MinecraftServer server;
-	private static ChunkyAPI chunkyAPI;
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		chunkyAPI = ChunkyProvider.get().getApi();
-		
 		ServerLifecycleEvents.SERVER_STARTING.register(server -> {
-            ChunkyPlayerCount.server = server;
-        });
+			ChunkyPlayerCount.server = server;
+		});
 
 		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
-            ServerPlayConnectionEvents.JOIN.register(this::onPlayJoin);
-            ServerPlayConnectionEvents.DISCONNECT.register(this::onPlayDisconnect);
-        });
+			ServerPlayConnectionEvents.JOIN.register(this::onPlayJoin);
+			ServerPlayConnectionEvents.DISCONNECT.register(this::onPlayDisconnect);
+		});
 	}
 
 	private void onPlayJoin(ServerPlayNetworkHandler handler, PacketSender sender, MinecraftServer server) {
-		chunkyAPI.pauseTask(getWorldName()); // handles if there is no task to pause
+		getChunkyAPI().pauseTask(getWorldName()); // handles if there is no task to pause
 	}
 
 	private void onPlayDisconnect(ServerPlayNetworkHandler handler, MinecraftServer server) {
-		if (serverEmpty()) {
-			chunkyAPI.continueTask(getWorldName()); // handles if there is no task to continue
+		if (serverSize() <= 1) { // counts player that just disconnected, race condition?
+			getChunkyAPI().continueTask(getWorldName()); // handles if there is no task to continue
 		}
 	}
 
 	private static MinecraftServer getServer() {
-        return server;
-    }
+		return server;
+	}
 
-	private boolean serverEmpty() {
-		return PlayerLookup.all(getServer()).isEmpty();
+	private int serverSize() {
+		return PlayerLookup.all(getServer()).size();
 	}
 
 	private String getWorldName() {
-		return getServer().getSavePath(WorldSavePath.ROOT).getParent().getFileName().toString();
+		return getServer().getWorld(World.OVERWORLD).getRegistryKey().getValue().toString();
+	}
+
+	// Chunky needs to be loaded
+	private ChunkyAPI getChunkyAPI() {
+		return ChunkyProvider.get().getApi();
 	}
 }
